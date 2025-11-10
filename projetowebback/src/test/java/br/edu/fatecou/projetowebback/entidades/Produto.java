a575cf0bb2c4d1d24de6fd165e5bf86b793b4edb
Augusto Farias de Lima <113651842+AugustoFL@users.noreply.github.com>
2025-11-10 17:34:33 +0000

    kjk#

---
+++ projetowebback/src/test/java/br/edu/fatecou/projetowebback/entidades/Produto.java
@@ -0,0 +1,9 @@
+package br.edu.fatecou.projetowebback.entidades;
+
+/**
+ * Arquivo de placeholder de testes. A entidade real foi movida para
+ * src/main/java para ser usada pela aplicação.
+ */
+public class Produto {
+    // placeholder para evitar conflitos em src/test durante o build
+}
